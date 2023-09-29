3339a1393c7ad9969da9093ce9f802235e8ea94c
7op3 <134126156+7op3@users.noreply.github.com>
2023-09-30 00:52:30 +0100

    added comments, may come back and add more detail l8r :3

--- basic calculator.py
+++ basic calculator.py
@@ -1,11 +1,13 @@
 #this is a program that will expand upon 'add.py'. it will still ask the user for two integer inputs, but it will also ask the user for a mathematical operation, and then it will carry that operation out.
 
+#takes two inputs, 'num1' and 'num2'. the user will have to input these themselves.
 num1 = int(input('Please enter a number.'))
-
 num2 = int(input('Please enter another number.'))
 
+#takes another input, which is the mathematical operation that will be used.
 operation = input('Please select a mathematical operator (+, - , * or /)')
 
+#these 'if' and 'elif' statements check which mathematical operator has been chosen, and then prints out the answer, or provides an error message if an operator isn't provided.
 if operation == '+':
     print(num1,operation,num2,'=',num1+num2)
 elif operation == '-':
