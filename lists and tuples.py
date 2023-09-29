9bb5bdeac0f316b2cdb1a189026e41b895561e3a
7op3 <ropecantcode@gmail.com>
2023-09-29 21:26:39 +0100

    added some examples with comments, adding more l8r

---
+++ lists and tuples.py
@@ -0,0 +1,14 @@
+#this is a program that will try to familiarize you with lists and tuples.
+
+#lists can be declared by using square brackets '[]' and can have data added and removed from them.
+Alist = [1,2,3,4]
+
+#lists can also be empty.
+Emptylist = []
+
+#tuples are like lists, exept that they are declared by using parenthesis '()' and cant have any data added and removed from them.
+Atuple = (5,6,7,8)
+
+#most programming languages (including python) start counting from 0.
+#the print statment below will print the first value from 'Alist', which is 1.
+print(Alist[0])
