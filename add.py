ce1bd0bf026140fcb8d2c036f4d38ee3ce334cf5
7op3 <ropecantcode@gmail.com>
2023-09-29 17:24:31 +0100

    added the final comments on the program.

--- add.py
+++ add.py
@@ -1,6 +1,8 @@
 #this is a program that asks a user for an two integer inputs, and then adds them together, printing the output.
 
+#takes two inputs 'num1' and 'num2'. the user will have to input these themselves.
 num1 = int(input('Please enter a number.'))
 num2 = int(input('Please enter another number.'))
 
+#uses concatenation to print both numbers and the sum of the numbers.
 print(num1,'+',num2,'=',num1+num2)
