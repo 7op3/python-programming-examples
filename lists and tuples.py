5101f47084c1261f7b9e772d3dbda0e0b851e344
7op3 <134126156+7op3@users.noreply.github.com>
2023-11-04 23:58:20 +0000

    Update lists and tuples.py
    added another example, will either add more later, or start on other stuff :)

--- lists and tuples.py
+++ lists and tuples.py
@@ -12,3 +12,7 @@ Atuple = (5,6,7,8)
 #most programming languages (including python) start counting from 0.
 #the print statment below will print the first value from 'Alist', which is 1.
 print(Alist[0])
+
+#to print the whole list, you just use 'print' and then the name of your list, like you're printing any other varianle.
+print(Alist)
+
